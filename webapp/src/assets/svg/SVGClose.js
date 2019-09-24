be8cbb8f77a8cd0329249a87aa1001acb5f48daa
Ignacio Mazzara <nachomazzara@gmail.com>
2019-09-24 16:05:47 -0300

    feat: desing svg;

--- webapp/src/assets/svg/SVGClose.js
+++ webapp/src/assets/svg/SVGClose.js
@@ -3,11 +3,11 @@ import React from 'react'
 const SVGDiv =  props => <svg  version="1.1" style={{'padding': '13px',
   'width': '13px',
   'height': '13px'}} {...props}>
-<line x1="1" y1="11"
-     x2="11" y2="1"
+<line x1="1" y1="9"
+     x2="9" y2="1"
      strokeWidth="1"/>
 <line x1="1" y1="1"
-     x2="11" y2="11"
+     x2="9" y2="9"
      strokeWidth="1"/>
 </svg>
 
