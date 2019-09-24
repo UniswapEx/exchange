4be96ea83300ca6fa89257df5b9f928683a1327a
Ignacio Mazzara <nachomazzara@gmail.com>
2019-09-24 16:08:41 -0300

    feat: design

--- webapp/src/assets/svg/SVGDiv.js
+++ webapp/src/assets/svg/SVGDiv.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const SVGClose =  props => <svg  version="1.1" style={{'padding': '13px',
-  'width': '13px',
-  'height': '13px'}} {...props}>
+  'width': '11px',
+  'height': '11px'}} {...props}>
 <line x1="1" y1="9"
      x2="9" y2="1"
      strokeWidth="1"/>
